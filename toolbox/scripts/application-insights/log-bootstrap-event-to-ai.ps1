3963384d89d3344bcb1b3e9ad9cdd01a0347ef1a
Willy-Peter Schaub <willy.schaub@worksafebc.com>
2022-11-04 13:02:43 -0700

    Add oss baseline files

---
+++ toolbox/scripts/application-insights/log-bootstrap-event-to-ai.ps1
@@ -0,0 +1,326 @@
+<#
+	.SYNOPSIS
+	Powershell Script to log telemetry to App Insights using REST API
+	
+	.DESCRIPTION
+        Supply the parameters and invoke the scripts. OperationId, action and Event are mandatory parameters 
+	
+	.USAGE EXAMPLE       
+         C:\temp\AI_script_version2.ps1 `
+                -OperationId '__TODO_ACTION_INSERT_OPERATION_ID__' `
+                -Action 'start' `
+                -Event 'Bootstrap' `
+                -BootstrapMode 'testmode' `
+                -ApplicationType 'dotnet' `
+                -ApplicationBlueprint 'generic' `
+                -PortfolioName 'CLOUD.Samples' `
+                -ProductName 'AzDOPSteps.Ops' `
+                -SourcesDirectory 'd:\a\1\s' `
+                -VerboseFlag 'true' `
+                -ForceCheck 'true' `
+                -Previews 'Previews text'               
+
+
+#>
+[CmdletBinding()]
+param (
+ [Parameter(Mandatory= $true)]
+ [string]$OperationId,
+
+ [Parameter(Mandatory= $true)]
+ [string]$Action,
+
+ [Parameter(Mandatory= $true)]
+ [string]$Event,
+
+ [Parameter(Mandatory= $false)]
+ [string]$BootstrapMode,
+
+ [Parameter(Mandatory= $false)]
+ [string]$ApplicationType,
+
+ [Parameter(Mandatory= $false)]
+ [string]$ApplicationBlueprint,
+
+ [Parameter(Mandatory= $false)]
+ [string]$PortfolioName,
+
+ [Parameter(Mandatory= $false)]
+ [string]$ProductName,
+
+ [Parameter(Mandatory= $false)]
+ [string]$SourcesDirectory,
+
+ [Parameter(Mandatory= $false)]
+ [string]$VerboseFlag,
+
+ [Parameter(Mandatory= $false)]
+ [string]$ForceCheck,
+
+ [Parameter(Mandatory= $false)]
+ [string]$Previews,
+
+ [Parameter(Mandatory= $false)]
+ [string]$PollingTimeout,
+
+ [Parameter(Mandatory= $false)]
+ [string]$ApiKey,
+
+ [Parameter(Mandatory= $false)]
+ [string]$SonarQube,
+
+ [Parameter(Mandatory= $false)]
+ [string]$ConfigMode,
+
+ [Parameter(Mandatory= $false)]
+ [string]$ProjectKey,
+
+ [Parameter(Mandatory= $false)]
+ [string]$ProjectName,
+ 
+ [Parameter(Mandatory= $false)]
+ [string]$ScannerMode,
+
+ [Parameter(Mandatory= $false)]
+ [string]$CliSources,
+
+ [Parameter(Mandatory= $false)]
+ [string]$Mutants,
+
+ [Parameter(Mandatory= $false)]
+ [string]$Survivors,
+
+ [Parameter(Mandatory= $false)]
+ [string]$DuplicateCode
+)
+
+<#
+    .SYNOPSIS
+        Function to build custom properties for Bootstrap Event.
+
+    .DESCRIPTION
+
+         Event 			Action				Parameters
+	Bootstrap	        Start                           BootstrapMode: <text>
+                                                                ApplicationType: <text>
+                                                                ApplicationBlueprint: <text>
+                                                                PortfolioName: <text>
+                                                                ProductName: <text>
+                                                                SourcesDirectory: <text>
+                                                                Verbose: <text>
+                                                                ForceCheck: <text>
+                                                                Previews: <text>
+
+        Bootstrap               Exit               
+#>
+
+Function LogBootstrapEvent () 
+{    
+    switch ($Action)
+    {
+        'Start'
+        {
+            $properties = [PSCustomObject]@{
+                Action               = $Action
+                Mode                 = $BootstrapMode
+                ApplicationType      = $ApplicationType
+                ApplicationBlueprint = $ApplicationBlueprint
+                PortfolioName        = $PortfolioName
+                ProductName          = $ProductName
+                SourcesDirectory     = $SourcesDirectory
+                Verbose              = $VerboseFlag
+                ForceCheck           = $ForceCheck
+                Previews             = $Previews
+            }
+        }
+        'Exit'
+        {
+            $properties = [PSCustomObject]@{
+                Action = $Action
+            }  
+        }   
+    }
+    return $properties
+}
+
+
+<#
+    .SYNOPSIS
+	Function to build custom properties for Building Code Event.
+
+    .DESCRIPTION
+
+          Event 			Action				Parameters
+	Building Code	                Stryker	                      Mutants: <text>
+                                                                      Survivors: <text>
+#>
+
+Function LogBuildingCodeEvent
+{
+    switch ($Action)
+    {
+        'Stryker'
+        {
+            $properties = [PSCustomObject]@{
+                Action    = $Action
+                Mutants   = $Mutants
+                Survivors = $Survivors 
+            }
+        }   
+        'Duplicate Code'
+        {
+            $properties = [PSCustomObject]@{
+                Action     = $Action
+                Duplicates = $DuplicateCode
+            }
+        }
+    }   
+    return $properties            
+}
+
+<#
+    .SYNOPSIS
+	Function to build custom properties for DevSecOps Event.
+
+    .DESCRIPTION
+
+         Event 			Action				                        Parameters
+	    DevSecOps	    SonarQube Prepare	                    SonarQube: <text>
+                                                                ScannerMode: <text>
+                                                                ProjectKey: <text>
+                                                                ProjectName: <text>
+                                                                -or-
+                                                                SonarQube: <text>
+                                                                ConfigMode: <text>
+                                                                ScannerMode: <text>
+                                                                ProjectKey: <text>
+                                                                ProjectName: <text>
+                                                                CliSources: <text>
+        DevSecOps	    SonarQube Analyze	                    PollingTimeout: <text>
+        DevSecOps	    SonarQube Publish	                    PollingTimeout: <text>
+	    DevSecOps	    Whitesource Scan	                    ApiKey: <text>
+#>
+
+Function DevSecOpsEvent () 
+{
+    switch ($Action)
+    {
+        'SonarQube Prepare'
+        {
+            if(![string]::IsNullOrEmpty($CliSources) -or ![string]::IsNullOrEmpty($ConfigMode) )
+            {
+                $properties = [PSCustomObject]@{
+                    Action      = $Action
+                    SonarQube   = $SonarQube
+                    ConfigMode  = $ConfigMode
+                    ProjectKey  = $ProjectKey
+                    ProjectName = $ProjectName
+                    ScannerMode =  $ScannerMode
+                    CliSources  = $CliSources
+                }
+            }
+            else
+            {
+                $properties = [PSCustomObject]@{
+                    Action      = $Action
+                    SonarQube   = $SonarQube
+                    ScannerMode = $ScannerMode
+                    ProjectKey  = $ProjectKey
+                    ProjectName = $ProjectName 
+                }                    
+            }
+        }
+    
+        'SonarQube Analyze'
+        {
+            $properties = [PSCustomObject]@{
+                Action = $Action
+            }
+        }
+
+        'SonarQube Publish'
+        {
+            $properties = [PSCustomObject]@{
+                Action         = $Action
+                PollingTimeout = $PollingTimeout
+            }
+        }
+
+        'Whitesource Scan'
+        {
+            $properties = [PSCustomObject]@{
+                Action = $Action
+                ApiKey = $ApiKey
+            }
+        }
+    }
+    return $properties      
+}
+
+######################################################################################################
+# MAIN SCRIPT
+######################################################################################################
+
+##Instrumentation key for DV-DV-CC-CommonEngineering-AI
+$InstrumentationKey = '__TODO_ACTION__INSERT_AI_INSTRUMENTATION_KEY__'
+
+#Build the custom properties
+$properties = switch ($Event)
+{
+    'Bootstrap' {LogBootstrapEvent}
+    'DevSecOps' {DevSecOpsEvent}
+    'Building Code' {LogBuildingCodeEvent}
+}
+
+#Build the request body
+$bodyObject = [PSCustomObject]@{
+    'name' = "Microsoft.ApplicationInsights.$InstrumentationKey.Event"
+    'time' = ([System.dateTime]::UtcNow.ToString('o'))
+    'iKey' = $InstrumentationKey
+    'tags' = [PSCustomObject]@{
+        'ai.operation.id' = $OperationId
+    }
+    'data' = [PSCustomObject]@{
+        'baseType' = 'EventData'
+        'baseData' = [PSCustomObject]@{
+            'ver' = '2'
+            'name' = $Event
+            'properties' = $properties
+        }
+    }
+}
+
+$bodyAsCompressedJson = $bodyObject | ConvertTo-JSON -Depth 10 -Compress
+$headers = @{'Content-Type' = 'application/x-json-stream';}
+
+# Invoke rest api call to log telemetry to Application insights
+
+# 2021.02.06 WS Add Retry count
+$retryCount = 3
+$retrySleep = 1.13
+$retryCheck = $retryCount - 1
+
+for ( $failureCount = 0; $failureCount -lt $retryCount; $failureCount++ )
+{
+    try
+    {
+        Invoke-WebRequest -Uri 'https://dc.services.visualstudio.com/v2/track' -Method 'POST' -headers $headers -body $bodyAsCompressedJson
+        break;
+    }
+    catch
+    {
+        if ( $failureCount -eq $retryCheck )
+        {
+            $errorMessage = "Retry count exceeded: " + $_.Exception.Message
+            Write-Host -BackgroundColor Red -ForegroundColor White $errorMessage
+            break;
+        }
+        else 
+        {
+            $retrySleep = $retrySleep * ( $failureCount + 1 );
+            $errorMessage = "Sleep and then retry. Count: " + $failureCount + " Sleep: " + $retrySleep +  " Error: " + $_.Exception.Message
+            Write-Host -BackgroundColor Yellow -ForegroundColor Black $errorMessage
+            Start-Sleep -Seconds $retrySleep
+        }
+    }
+}
---
+++ toolbox/scripts/building-code/send-email.ps1
@@ -0,0 +1,128 @@
+﻿<#
+	.NAME
+    Send-Mail.ps1
+
+    .SYNOPSIS
+	Powershell Script to Send Email using office 365 smtp settings
+	
+	.DESCRIPTION
+    Supply the parameters and invoke the scripts. To,From,message are mandatory parameters and others are optional
+	
+	.USAGE EXAMPLE       
+         C:\temp\Send-Mail.ps1 `
+                -To 'TO' `
+                -From 'SUBJECT' `
+                -Subject 'FROM' `
+                -body 'BODY' `
+
+Calling Syntax #.\Send-Mail.ps1 -To "target@demo.com" -Subject "test subject" -From "source@demo.com" -Password "xxxxxx" -Body "Test body"              
+
+#>
+
+####### Set Parameters ######
+Param(
+     [Parameter(Mandatory=$false)]
+     [string]
+     $To,
+
+     [Parameter(Mandatory=$false)]
+     [string]
+     $Subject,
+
+     [Parameter(Mandatory = $false)]
+     [string]
+     $From,
+
+     [Parameter(Mandatory = $false)]
+     [String]
+     $Password,
+
+     [Parameter(Mandatory = $false)]
+     [string]
+     $Body
+)
+
+Write-Verbose "$((Get-Date).ToShortDateString()) : Started running $($MyInvocation.MyCommand)"
+
+#----------------------
+# Add common functions
+#----------------------
+. $PSScriptRoot\functions.ps1
+#------------------------------------------------------------------
+
+Write-Log -LineValue "Starting Script..."
+### Script Global Settings
+$creds = New-Object System.Management.Automation.PSCredential -ArgumentList $From, $($password | ConvertTo-SecureString -AsPlainText -Force)
+
+#Declare SMTP Connection Settings
+$smtpConnection = @{
+    # Office365 SMTP Relay FQDN
+    smtpServer = 'smtp.office365.com'
+    port = 587 
+    useSsl = $true
+    credential = $creds
+}
+
+### Script Variables
+#Declare Mailmessages.
+$mailMessage = @{
+    from = $From
+    to = @(
+        $To
+    )
+    cc = @(
+        "CC@EMAIL.COM"
+    )
+    bcc = @(
+        "BCC@EMAIL.COM"
+    )
+    subject = 'INFO-SonarQube Metrices'
+    
+    priority = 'High' #Normal by default, options: High, Low, Normal
+
+    bodyAsHtml = $true    
+
+    body = "Something Unexpected Occured as no Content has been Provided for this Mail Message!" #Default Message
+}
+
+### Script Start
+
+#Set Mail Body and store it as HTML with Special CSS Class
+$messageBody_HTLM = $Body
+
+#Retrieve CSS Stylesheet
+$css = Invoke-WebRequest "https://raw.githubusercontent.com/advancedrei/BootstrapForEmail/master/Stylesheet/bootstrap-email.min.css"  -UseBasicParsing | Select-Object -ExpandProperty Content
+
+#Build HTML Mail Message and Apply it to the MailMessage HashTable
+$mailMessage.body = ConvertTo-Html -Title $MailMessage.subject -Head "<style>$($CSS)</style>" $Body "
+    <p>
+        Worksafe BC Devops,    
+    </p>
+
+    <p>
+        Attention - Please refer quality gate parametres related to code duplications for your project.</br>
+        </br>
+        <div class='alert alert-info' role='alert'>
+            SonarQube Code Duplicate Report
+        </div>
+        $($messageBody_HTLM)
+    </P>
+" | Out-String
+
+
+#Send MailMessage
+#This example uses the HashTable's with a technique called Splatting to match/bind the Key's in the HashTable with the Parameters of the command.
+#Use the @ Symbol instead of $ to invoke Splatting, Splatting improves readability and allows for better management and reuse of variables
+
+try
+{
+    Send-MailMessage @smtpConnection @mailMessage
+}
+catch
+{
+    Write-Host "Send Email Failed"
+    Write-Host "##vso[task.complete result=Failed;]FAILED"
+    $errorMessage = $_.Exception.Message
+    Write-Log -LineValue $_.Exception    
+    return $errorMessage  
+}
